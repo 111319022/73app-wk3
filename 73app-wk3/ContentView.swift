4eafcb8013f1bf0840b4954ff2d687d2bdd1fac4
Ray Hsu (MacBook Air M3) <s111319022@stu.ntue.edu.tw>
2026-03-14 20:05:03 +0800

    Initial Commit

---
+++ 73app-wk3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  73app-wk3
+//
+//  Created by Ray Hsu on 2026/3/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 73app-wk3/_3app_wk3App.swift
@@ -0,0 +1,17 @@
+//
+//  _3app_wk3App.swift
+//  73app-wk3
+//
+//  Created by Ray Hsu on 2026/3/14.
+//
+
+import SwiftUI
+
+@main
+struct _3app_wk3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
